d78aea1361199a3ada0fa21d08d3da96dbadd982
Severiano Badajoz <sevebadajoz@gmail.com>
2018-01-31 15:28:29 -0800

    Fix post4 function

--- Project1/Functions.cpp
+++ Project1/Functions.cpp
@@ -21,18 +21,22 @@ bool DArray::more1than0() {
 	int num1 = 0;
 	for (int i = 0; i < numOfElements; i++) {
 		num0 += a[i] == 0;
-		num1 += a[i] == 0;
+		num1 += a[i] == 1;
 	}
 	return num1 > num0;
 }
 
 
 // Definition function post4
-void DArray::post4(DArray& other) const{
+void DArray::post4(DArray& other) const {
 	int indexOf4;
-	for (int i = numOfElements - 1; i > -1; i--) 
-		if (a[i] == 4) indexOf4 = i;
-	for (int i = 0; i < indexOf4; i++) {
+	for (int i = numOfElements - 1; i > -1; i--) {
+	if (a[i] == 4) {
+		indexOf4 = i;
+		i = -1;
+	}
+}
+	for (int i = 0; i < numOfElements - indexOf4 - 1; i++) {
 		other.a[i] = a[indexOf4 + 1 + i];
 		other.numOfElements += 1;
 	}
